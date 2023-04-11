032a5c5523dcf2781ac2b0f9cd3e2058c3df3182
uptownkiddo <muchoki.lewis8@gmail.com>
2023-04-10 19:18:51 -0700

    more data structures

---
+++ 0x04-python-more_data_structures/Python.h
@@ -0,0 +1,22 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+def square_matrix_simple(matrix=[]);
+def search_replace(my_list, search, replace);
+def uniq_add(my_list=[]);
+def common_elements(set_1, set_2);
+def only_diff_elements(set_1, set_2);
+def number_keys(a_dictionary);
+def print_sorted_dictionary(a_dictionary);
+def update_dictionary(a_dictionary, key, value);
+def simple_delete(a_dictionary, key="");
+def multiply_by_2(a_dictionary);
+def best_score(a_dictionary);
+def multiply_list_map(my_list=[], number=0);
+def weight_average(my_list=[]);
+def square_matrix_map(matrix=[]);
+def complex_delete(a_dictionary, value);
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+#endif
